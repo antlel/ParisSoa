8e28d8bb2a680ebd61a933775134b0d1182df6f6
Nicolas JANSSOONE <nicolas.janssoone@epsi.fr>
2016-04-15 11:08:59 +0200

    Ajout de la base de controleur.

---
+++ Service/Controller/BaseController.php
@@ -0,0 +1,72 @@
+<?php
+namespace Service\Controller;
+
+/**
+ * Classe de base des contrôleurs
+ */
+class BaseController implements IServiceController
+{
+
+    public function GET($params=NULL){
+		$this->redirectTo404();
+	}
+
+	/**
+	 * Action a faire après un appelle en POST
+	 * @throws Exeption non implémenté
+	 */
+	public function POST(){
+		$this->redirectTo404();
+	}
+
+	/**
+	 * Action a faire après un appelle en DELETE
+	 * @throws Exeption non implémenté
+	 */
+	public function DELETE($params=null){
+		$this->redirectTo404();
+	}
+
+	/**
+	 * Action a faire après un appelle en PUT
+	 * @throws Exeption non implémenté
+	 */
+	public function PUT($params=null){
+		$this->redirectTo404();
+	}
+
+	private function redirectTo404(){
+		$exception = new notImplementException(__LINE__, __FILE__, __CLASS__, __METHOD__, "erreur");
+		$this->response($exception->getRepresentativeObject(),404);
+	}
+
+	public function response($string, $http){
+		$sessionService = new sessionService();
+		header("contractVersionNumber :".$sessionService->getVersion(), TRUE);
+		header('content-type: application/json; charset=utf-8');
+		switch ($http) {
+			case 200:
+				header("HTTP/1.0 200 Ok");
+				break;
+			case 500:
+				header("HTTP/1.0 500 Internal Error");
+				break;
+			case 404:
+				header("HTTP/1.0 404 Not Found");
+				break;
+		}
+		echo json_encode($string);
+	}
+
+	public function isTokenValid($userToken){
+		//case of missed userToken
+		if(!isset($userToken)){
+			return FALSE;
+		}
+		//secure string of userToken
+		$userToken = addslashes($userToken);
+		//check validity
+		$tok = new userTokenService();
+		return $tok->isTokenValid($userToken);
+	}
+}
---
+++ Service/Controller/IServiceController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Service\Controller;
+
+interface IServiceController
+{
+    public function GET();
+    public function POST();
+    public function PUT();
+    public function DELETE();
+}
