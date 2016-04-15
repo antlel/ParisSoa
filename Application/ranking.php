374dbcba7f7c1afb182060fd77cbebcf66a6d2e0
Armand SZYPURA <armand@skeatwin.fr>
2016-04-15 11:41:42 +0200

    Ajout de l'application v1.0

---
+++ Application/ranking.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Euro 2016 - Classement des joueurs</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="css/user.css"/>
+		<link rel="start" title="Accueil" href="index.php"/>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<link rel="icon" type="image/png" href="img/favicon.png"/>s
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</head>
+	<body role="document">
+		<?php include('templates/header.php') ?>
+
+		<div class="container vertical-offset-50">
+			<div class="page-header">
+				<h2>Classement des joueurs</h2>
+			</div>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th>Rang</th>
+						<th>Prénom</th>
+						<th>Nom</th>
+						<th>Points</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Armand</td>
+						<td>SZYPURA</td>
+						<td>512</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Armand</td>
+						<td>SZYPURA</td>
+						<td>512</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>Armand</td>
+						<td>SZYPURA</td>
+						<td>512</td>
+					</tr>
+				</tbody>
+			</table>
+			
+			<?php include('templates/footer.php') ?>
+		</div>
+	</body>
+</html>
---
+++ Application/templates/header.php
@@ -0,0 +1,25 @@
+<!-- Fixed navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php"><image src="img/logo.min.png"/></a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse" style="padding-top: 6px">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php">Matchs disponibles</a></li>
+				<li><a href="ranking.php">Classement</a></li>
+				<li><a href="matchs.php">Gestion des matchs</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="nav-username">Armand SZYPURA -</li>
+	        	<li><a href="login.php">Se déconnecter</a></li>
+	        </ul>
+		</div>
+	</div>
+</nav>
